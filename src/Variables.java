b00f14196b9b1d2aad24efd2418a846064e3f5d6
Maxim Mineev <mms1401@ya.ru>
2024-06-17 12:08:39 +0500

    lessons java

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 3266L;
+
+        double myDouble = 3266.5;
+        float myFloat = 3266.5f;
+
+        char myChar = 'a';
+        boolean myBoolean = true;
+
+        byte myByte = 127;
+        System.out.println(myInt);
+    }
+}
