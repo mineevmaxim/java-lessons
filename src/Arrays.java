7f3642070a7a51fc57c645b1324eb0b858249f2a
Maxim Mineev <mms1401@ya.ru>
2024-06-17 13:00:33 +0500

    array

---
+++ src/Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int number = 10;
+        char character = 'a';
+        String string = "Hello";
+        int[] numbers = new int[5];
+        int[] nums = {1, 2, 3};
+        System.out.println(numbers[1]);
+    }
+}
