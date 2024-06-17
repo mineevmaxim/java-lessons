1982cf71280bf2cac04cfa4ee9149c73b65edb7d
Maxim Mineev <mms1401@ya.ru>
2024-06-17 12:39:13 +0500

    system in

--- src/If.java
+++ src/If.java
@@ -1,5 +1,11 @@
 public class If {
     public static void main(String[] args) {
-        
+        if (5 < 4) {
+            System.out.println("true");
+        } else if (5 < 3 || 5 > 4 && 5 > 3) {
+            System.out.println("false");
+        } else {
+            System.out.println("ahahha");
+        }
     }
 }
---
+++ src/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String string = s.nextLine();
+        System.out.println(string);
+        int x = s.nextInt();
+        System.out.println(x);
+    }
+}
