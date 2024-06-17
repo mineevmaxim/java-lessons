fe2c0bc6453744d00a4542b0377bd85d899e25df
Maxim Mineev <mms1401@ya.ru>
2024-06-17 12:30:50 +0500

    loops

---
+++ src/ForLoops.java
@@ -0,0 +1,7 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        for (var i = 0; i <= 10; i++) {
+            System.out.println("Hello World " + i);
+        }
+    }
+}
--- src/Strings.java
+++ src/Strings.java
@@ -1,6 +1,10 @@
 public class Strings {
     public static void main(String[] args) {
         String myString = "Hello";
-        
+        String space = " ";
+        String name = "World";
+        System.out.println(myString + space + name);
+        System.out.println("Hello" + " " + "Worlds");
+        System.out.println("2 + 2 = " + (2 + 2));
     }
 }
